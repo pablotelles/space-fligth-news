ec526c9688e915636ba383b83cdd30ea6488a5f7
Pablo Teles <88568500+pablotelles@users.noreply.github.com>
2022-04-12 17:39:30 -0300

    some-ajustments-on-Contxt

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { Home } from "./pages/Home.jsx";
 import { DataProvider } from "./contexts/dataContext.jsx";
 
+
 function App() {
   return (
     <DataProvider>
-    <div className="App">
-      <Home />
-    </div>
+        <div className="App">
+          <Home />
+        </div>
     </DataProvider>
   );
 }
